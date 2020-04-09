e6fccf0152f07c0c259e47a83242da0025b630ba
kaline <mesquitabfkaline@gmail.com>
2020-04-08 22:17:53 -0300

    update

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.post('/submit', (req,res)=> {
         })
     })
 })
-app.listen(3000, function(Port){
-    Port = 3000;
-    console.log('Port: '+ Port)
-})
+aapp.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
